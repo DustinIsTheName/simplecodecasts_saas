8a187a44ccd7a9d4900f2281927656ca90d95751
Dustin Milam <darklordsushi@yahoo.com>
2015-12-16 15:39:48 -0500

    Added about page view, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get '/about' => 'pages#about'
+
   root 'pages#home'
 
   # Example of regular route:
